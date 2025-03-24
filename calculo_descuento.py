4947b9fd23037ff5e2083f1d3b7c7d711916564f
BryanEscobar10 <bd.escobars@uea.edu.ec>
2025-03-24 13:32:50 -0800

    calcular_descuento

--- calculo_descuento.py
+++ calculo_descuento.py
@@ -1,13 +1,15 @@
-def calcular_descuento(monto_total_compra,porcentaje_descuento):
+def calcular_descuento(monto_total_compra,porcentaje_descuento):   #funcion para calcular descuento
     descuento= (monto_total_compra*porcentaje_descuento)/100
     return descuento
 
-while True:
+def menu():         #funcion menu del sistema
     print("------Calculo de descuento a compras realizadas------")
     print("Elija una opcion:")
     print("1.-Carcular descuento")
     print("2.- Salir del sistema")
 
+while True:
+    menu()
     opcion= int(input("Ingrese la opcion a realizar:  "))
 
     if opcion == 2:
@@ -15,12 +17,13 @@ while True:
         break
 
     elif opcion == 1:
-        monto= float(input("Precio original del producto:  "))
-        descuento= float(input("Ingrese el porcentje de descuento:  "))
-        cantidad_descuento = calcular_descuento(monto,descuento)
-        print(f"La cantidad del descuento es: {cantidad_descuento}")
+        monto= float(input("Precio original del producto:  "))    #ingreso del precio original
+
+        descuento= float(input("Ingrese el porcentje de descuento:  "))  #ingreso del porcentaje de descuento
+        cantidad_descuento = calcular_descuento(monto,descuento)         #llamar a la funcion
+        print(f"La cantidad del descuento es: {cantidad_descuento}")    #cantidad de descuento
         total_pagar= monto-cantidad_descuento
-        print(f"Precio final a pagar con descuento: {total_pagar}")
+        print(f"Precio final a pagar con descuento: {total_pagar}")     #precio final aplicado el descuento
     else:
         print("Opcion invalida")
 
